8b2d290ef900e698f2cbbd9282b6bfdfe471eaca
muiruriscode <percymuiruris2@gmail.com>
2023-10-10 23:19:38 +0300

    added shadow to medel

--- main.js
+++ main.js
@@ -14,10 +14,29 @@ camera.position.set(0, 0.5, 2);
 const renderer = new THREE.WebGLRenderer();
 renderer.setClearColor(0x111114);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 const loadingManager = new THREE.LoadingManager();
+
+scene.add(new THREE.AmbientLight(0xffffff, 0.3));
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 2, 0);
+light.castShadow = true;
+scene.add(light);
+
+light.shadow.mapSize.width = 512;
+light.shadow.mapSize.height = 512;
+light.shadow.camera.near = 0.5;
+light.shadow.camera.far = 500;
+
+const spotLight = new THREE.SpotLight(0xffffff);
+scene.add(spotLight);
+spotLight.position.set(0, 2, 0);
+spotLight.intensity = 1.0;
+spotLight.angle = 0.45;
+spotLight.penumbra = 0.7;
 
 const progressBar = document.getElementById("progress-bar");
 const container = document.getElementById("container");
@@ -35,32 +54,37 @@ loadingManager.onError = function () {
   console.error("Error");
 };
 
-const gltfloader = new FBXLoader(loadingManager);
+const fbxfloader = new FBXLoader(loadingManager);
 
 let mixer;
-gltfloader.load("/model3/Run.fbx", function (gltf) {
-  gltf.scale.set(0.005, 0.005, 0.005);
-  mixer = new THREE.AnimationMixer(gltf);
-  const clips = gltf.animations;
+fbxfloader.load("/model3/Run.fbx", function (model) {
+  model.scale.set(0.005, 0.005, 0.005);
+
+  model.traverse(function (node) {
+    if (node.isMesh) {
+      node.castShadow = true;
+    }
+  });
+
+  mixer = new THREE.AnimationMixer(model);
+  const clips = model.animations;
   const clip = THREE.AnimationClip.findByName(clips, "mixamo.com");
   const action = mixer.clipAction(clip);
   action.play();
-  scene.add(gltf);
+  scene.add(model);
 });
 
-scene.add(new THREE.AmbientLight(0xffffff, 1.0));
-const light = new THREE.DirectionalLight(0xffffff, 1.0);
-light.position.set(0, 10, 10);
-scene.add(light);
-
-const geometry = new THREE.PlaneGeometry(5, 5);
-const material = new THREE.MeshBasicMaterial({ color: 0x222222 });
+const geometry = new THREE.PlaneGeometry(20, 20, 32, 32);
+const material = new THREE.MeshStandardMaterial({
+  color: 0x444444,
+});
 const mesh = new THREE.Mesh(geometry, material);
 mesh.rotation.x = -Math.PI / 2;
+mesh.receiveShadow = true;
 scene.add(mesh);
 
 const clock = new THREE.Clock();
-function animate(time) {
+function animate() {
   window.requestAnimationFrame(animate);
   if (mixer) mixer.update(clock.getDelta() / 5);
   controls.update();
